1474950172b52f1bef40dece64beacd2afd299d5
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-20 20:53:29 -0600

    Fix indentation

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,34 +3,34 @@ require 'minitest/pride'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
 
-     def setup
-          @cruiser = Ship.new("Cruiser", 3)
-     end
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
 
-     def test_it_exists
-          assert_instance_of Ship, @cruiser
-     end
+  def test_it_has_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
+  end
 
-     def test_it_has_attributes
-          assert_equal "Cruiser", @cruiser.name
-          assert_equal 3, @cruiser.length
-          assert_equal 3, @cruiser.health
-     end
+  def test_method_sunk_initial
+    assert_equal false, @cruiser.sunk?
+  end
 
-     def test_method_sunk_initial
-          assert_equal false, @cruiser.sunk?
-     end
+  def test_method_hit
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+  end
 
-     def test_method_hit
-          @cruiser.hit
-          assert_equal 2, @cruiser.health
-     end
-
-     def test_method_sunk_true
-          @cruiser.hit
-          @cruiser.hit
-          assert_equal true, @cruiser.sunk?
-          assert_equal 0, @cruiser.health # do we need this test?
-     end
+  def test_method_sunk_true
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
+    assert_equal 0, @cruiser.health # do we need this test?
+  end
 end
