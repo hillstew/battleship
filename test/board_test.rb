838244f559d3f7217261f0f7e3932fae5ae8ad83
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-21 13:23:59 -0600

    Add test_it_has_cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,12 +5,20 @@ require './lib/cell'
 require './lib/board'
 
 class BoardTest < Minitest::Test
+
   def setup
-    @board = Board.new()
+    @board = Board.new
   end
 
   def test_it_exists
     assert_instance_of Board, @board
   end
 
+  def test_it_has_cells
+    assert_instance_of Hash, @board.cells
+    assert_equal 16, @board.cells.count
+    @board.cells.values.each do |value|
+      assert_instance_of Cell, value
+    end
+  end
 end
