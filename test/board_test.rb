9ae6373f4791ea9e72ac5770eebdf25828cb1488
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-22 13:42:56 -0600

    Update board_test.rb to test new initialize method for infinite board

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,7 +7,8 @@ require './lib/board'
 class BoardTest < Minitest::Test
 
   def setup
-    @board = Board.new
+    # @board = Board.new
+    @board = Board.new(10, "F") #infinite board
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
@@ -18,36 +19,37 @@ class BoardTest < Minitest::Test
 
   def test_it_has_cells
     assert_instance_of Hash, @board.cells
-    assert_equal 16, @board.cells.count
+    assert_equal 60, @board.cells.count
+
     @board.cells.values.each do |value|
-      assert_instance_of Cell, value
+    assert_instance_of Cell, value
     end
   end
 
   def test_it_can_validate_a_coordinate
     assert_equal true, @board.valid_coordinate?("A1")
     assert_equal true, @board.valid_coordinate?("D4")
-    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("FF1")
     assert_equal false, @board.valid_coordinate?("A22")
   end
 
   def test_it_can_validate_placement_based_on_length
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
-    assert_equal true, @board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
   end
 
   def test_it_can_validate_placement_if_coordinates_are_consecutive
-    assert_equal false, @board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal false, @board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal false, @board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    assert_equal false, @board.valid_placement?(submarine, ["C1", "B1"])
-    assert_equal true, @board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 
   def test_it_can_validate_placement_if_coordinates_are_diagonal
-    assert_equal false, @board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    assert_equal false, @board.valid_placement?(submarine, ["C2", "D3"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_it_can_place_a_ship
