fe6fe3c89e4ecb5ec5bf0b1e9536d8663fb98ce8
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-23 11:55:23 -0600

    Fix bug in x_test_consecutive method

--- lib/board.rb
+++ lib/board.rb
@@ -32,9 +32,7 @@ class Board
 
     #build hash of values
     @y_range.each do |y|
-         # require 'pry'; binding.pry
          @x_range.each do |x|
-              # binding.pry
               @cells[y + (x).to_s] = Cell.new(y + (x).to_s)
          end
     end
@@ -46,40 +44,12 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
+    # require 'pry'; binding.pry
     if ship.length == coordinates.length
 
-         #test x/y axis consecutive
-         x_range_index = @x_range.index(get_x_coordinate(coordinates[0]))
-         x_test_consecutive = true
-         coordinates.each_index do |c_index|
-              if get_x_coordinate(coordinates[c_index]) != @x_range[x_range_index]
-                   x_test_consecutive = false
-                   x_range_index += 1
-              end
-         end
-
-         y_range_index = @y_range.index(get_y_coordinate(coordinates[0]))
-         y_test_consecutive = true
-         coordinates.each_index do |c_index|
-              if get_y_coordinate(coordinates[c_index]) != @y_range[y_range_index]
-                   y_test_consecutive = false
-                   y_range_index += 1
-              end
-         end
-
-
-
-
-         # x/y coordinates same
-         x_coordinate = get_x_coordinate(coordinates[0])
-         y_coordinate = get_y_coordiante(coordinates[0])
-
-         x_test_same = (coordinates.find_all {|coordinate| get_x_coordinate == x_coordinate}).length == ship.length
-         y_test_same = (coordinates.find_all {|coordinate| get_y_coordinate == y_coordinate}).length == ship.length
-
-         if x_test_consecutive && y_test_same
+         if x_test_consecutive(coordinates) && y_test_same(ship, coordinates)
               true
-         elsif x_test_same && y_test_consecutive
+         elsif x_test_same(ship, coordinates) && y_test_consecutive(coordinates)
               true
          else
               false
@@ -95,7 +65,50 @@ class Board
 
   def get_y_coordinate(coordinate)
        coordinate.delete('0-9')
- end
+  end
 
+  #test x/y axis consecutive
+  def x_test_consecutive(coordinates)
+     x_range_index = @x_range.index(get_x_coordinate(coordinates[0]).to_i)
+     x_test_consecutive = true
+     coordinates.each_index do |c_index|
+          if get_x_coordinate(coordinates[c_index]).to_i != @x_range[x_range_index]
+               x_test_consecutive = false
+          end
+          x_range_index += 1
+     end
+
+     x_test_consecutive
+  end
+
+  def y_test_consecutive(coordinates)
+     y_range_index = @y_range.index(get_y_coordinate(coordinates[0]))
+
+     y_test_consecutive = true
+     coordinates.each_index do |c_index|
+          if get_y_coordinate(coordinates[c_index]) != @y_range[y_range_index]
+             y_test_consecutive = false
+          end
+          y_range_index += 1
+     end
+
+     y_test_consecutive
+  end
+
+  def x_test_same(ship, coordinates)
+    x_test_same = coordinates.find_all do |coordinate|
+      get_x_coordinate(coordinate) == get_x_coordinate(coordinates[0])
+    end
+
+    x_test_same.length == ship.length
+  end
+
+  def y_test_same(ship, coordinates)
+    y_test_same = coordinates.find_all do |coordinate|
+      get_y_coordinate(coordinate) == get_y_coordinate(coordinates[0])
+    end
+
+    y_test_same.length == ship.length
+  end
 
 end
