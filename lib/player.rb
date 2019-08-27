02d44f8c1d6db7304a1fd093a75fdb8a74709177
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-27 15:39:10 -0600

    Update Player class for computer ship placement

--- lib/player.rb
+++ lib/player.rb
@@ -2,11 +2,19 @@ require './lib/board'
 require 'pry'
 
 class Player
-  attr_reader :shots, :board
+  attr_reader :shots, :board, :ships
 
   def initialize(computer=false)
     @computer = computer
-    @shots = []
+    @turns = []
     @board = Board.new(10, "F")
+    @ships = []
+    @current_turn = false
+  end
+
+  def all_sunk?
+    @ships.all? do |ship|
+      ship.sunk?
+    end
   end
 end
