3523dc1fd6b29b1d939f820bcfcc1c774a1e77e6
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-27 08:29:23 -0600

    Add Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+# User board is displayed showing hits, misses, sunken ships, and ships
+# Computer board is displayed showing hits, misses, and sunken ships
+# Computer chooses a random shot
+# Computer does not fire on the same spot twice
+# User can choose a valid coordinate to fire on
+# Entering invalid coordinate prompts user to enter valid coordinate
+# Both computer and player shots are reported as a hit, sink, or miss
+# User is informed when they have already fired on a coordinate
+# Board is updated after a turn
+
+class Turn
+
+end
