242b91e92f978579d69969bb8982e2d4ec0175a6
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-27 15:40:57 -0600

    Add Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,5 +9,65 @@
 # Board is updated after a turn
 
 class Turn
+  attr_reader :player
+
+  def initialize(player, computer)
+    @shots = []
+    @player = player
+    @computer = computer
+  end
+  # gathering info to fire coordinate
+  # execute shot
+  # report result
+
+  def get_coordinate_to_fire_on
+    coordinate_message
+    coordinate = gets.chomp.split
+
+    while @computer.board.valid_coordinate?(coordinate) == false
+      puts "Please enter a valid coordinate:"
+      coordinate_message
+      coordinate = gets.chomp.split
+    end
+
+    @computer.board.find do |cell|
+      if cell == coordinate && !cell.fired_upon?
+        cell.fire_upon
+      else
+        # message already been fired_upon
+      end
+
+    end
+    computer_take_shot
+  end
+
+  def show_boards
+    puts "=============COMPUTER BOARD=============="
+    print @computer.board.render
+    puts "=============PLAYER BOARD=============="
+    print @player.board.render(true)
+  end
+
+  def computer_take_shot
+    shot = generate_random_shot
+
+    @player.board.find do |cell|
+      if cell == shot && !cell.fired_upon?
+        cell.fire_upon
+      else
+        # message already been fired_upon
+      end
+    end
+  end
+
+  def generate_random_shot
+
+  end
+
+  def coordinate_message
+    puts "Enter the coordinate for your shot:"
+    print ">"
+  end
+
 
 end
