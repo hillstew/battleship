1d89ca9752c6061667a25cf75f88fc7566f6b17c
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-27 19:05:05 -0600

    Add feedback for shots in Turn and remove comments

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,4 @@
-# User board is displayed showing hits, misses, sunken ships, and ships
-# Computer board is displayed showing hits, misses, and sunken ships
-# Computer chooses a random shot
-# Computer does not fire on the same spot twice
-# User can choose a valid coordinate to fire on
-# Entering invalid coordinate prompts user to enter valid coordinate
-# Both computer and player shots are reported as a hit, sink, or miss
-# User is informed when they have already fired on a coordinate
-# Board is updated after a turn
+require 'pry'
 
 class Turn
   attr_reader :player
@@ -15,59 +7,80 @@ class Turn
     @shots = []
     @player = player
     @computer = computer
+    @hit_message = nil
   end
-  # gathering info to fire coordinate
-  # execute shot
-  # report result
 
   def get_coordinate_to_fire_on
     coordinate_message
-    coordinate = gets.chomp.split
+    coordinate = gets.chomp
 
-    while @computer.board.valid_coordinate?(coordinate) == false
+    unless @computer.board.valid_coordinate?(coordinate)
       puts "Please enter a valid coordinate:"
-      coordinate_message
-      coordinate = gets.chomp.split
+      coordinate = gets.chomp
     end
 
-    @computer.board.find do |cell|
-      if cell == coordinate && !cell.fired_upon?
-        cell.fire_upon
+    cell = @computer.board.cells.values_at(coordinate)
+    if cell[0].coordinate == coordinate && cell[0].fired_upon? == false
+      cell[0].fire_upon
+      if cell[0].render == "X"
+        @hit_message = "Your shot of #{coordinate} results in a sunk ship!"
+      elsif cell[0].render == "H"
+        @hit_message = "Your shot at #{coordinate} made a hit."
       else
-        # message already been fired_upon
+        @hit_message = "Your shot on #{coordinate} was a miss."
       end
-
+    else
+      @hit_message = "Your shot of #{coordinate} has already been fired upon."
     end
+    puts ""
+    puts @hit_message
+    puts ""
     computer_take_shot
-  end
-
-  def show_boards
-    puts "=============COMPUTER BOARD=============="
-    print @computer.board.render
-    puts "=============PLAYER BOARD=============="
-    print @player.board.render(true)
-  end
-
-  def computer_take_shot
-    shot = generate_random_shot
-
-    @player.board.find do |cell|
-      if cell == shot && !cell.fired_upon?
-        cell.fire_upon
-      else
-        # message already been fired_upon
-      end
-    end
-  end
-
-  def generate_random_shot
-
   end
 
   def coordinate_message
     puts "Enter the coordinate for your shot:"
     print ">"
   end
+
+  def show_boards
+    puts "=============COMPUTER BOARD=============="
+    print @computer.board.render
+    puts "=============PLAYER BOARD================"
+    print @player.board.render(true)
+  end
+
+  def computer_take_shot
+    shot = generate_random_shot
+    cell = @player.board.cells.values_at(shot)
+    binding.pry
+    unless cell[0].hit_status
+      shot = generate_random_shot
+    end
+
+    if cell[0].coordinate == shot && cell[0].fired_upon? == false
+      cell[0].fire_upon
+      if cell[0].render == "X"
+        @hit_message = "Computer shot of #{shot} results in a sunk ship!"
+      elsif cell[0].render == "H"
+        @hit_message = "Computer shot at #{shot} made a hit."
+      else
+        @hit_message = "Computer shot on #{shot} was a miss."
+      end
+    end
+    puts ""
+    puts @hit_message
+    puts ""
+  end
+
+  def generate_random_shot
+    random_x = @player.board.x_range.sample.to_s
+    random_y = @player.board.y_range.sample.to_s
+
+    random_y + random_x
+  end
+
+
 
 
 end
